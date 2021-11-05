4ee845910bdae790c242776ec0bbd75353ab5e68
Cuttlefeeesh <greenlightrj13@gmail.com>
2021-11-05 17:51:39 -0230

    removed welcome print statement

--- main.py
+++ main.py
@@ -103,14 +103,9 @@ class Controller:
                 window.view.height = event.h
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     MainWindow = AntGame()
     MainWindow.main_loop()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
