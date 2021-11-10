d01a300acb635bcfb79e775d406cdcd33264ccfd
Cuttlefeeesh <greenlightrj13@gmail.com>
2021-11-10 16:07:55 -0330

    rainbow confetti and trail demos

--- AntPheromoneClass.py
+++ AntPheromoneClass.py
@@ -1,5 +1,3 @@
-import PIL
-from PIL import Image
 import numpy as np
 import pygame
 import scipy.ndimage.filters
@@ -16,21 +14,15 @@ class AntPheromone:
         """
         self.name = name
         self.array = np.zeros((width, height,3))
-        self.dissipation_factor = 7
-        self.decay_factor = 0.1
+        self.dissipation_factor = 0.4
+        self.decay_factor = 0.999
 
-    def image(self):
-        """
-        :return: an image of the pheromone density
-        """
-        return PIL.Image.fromarray(self.array, "RGB")
-
-    def draw(self, pixels, color):
+    def draw(self, pixels: list, color: tuple):
         """
         updates array
         :param pixels: list of (x,y) pixel positions
         :param color: (R,G,B) color on a scale from 0 to 255
-        :return:updated array
+        :return: updated array
         """
         pixel = None
         for pixel in pixels:
@@ -44,19 +36,23 @@ class AntPheromone:
 
     def decay(self):
         blurred = scipy.ndimage.filters.gaussian_filter(self.array, sigma=self.dissipation_factor)
-        subtractor = self.decay_factor*np.ones(np.shape(self.array))
-        self.array = blurred - subtractor
+        subtractor = self.decay_factor*blurred#np.ones(np.shape(self.array))
+        self.array =  subtractor
 
 if __name__ == '__main__':
     done: bool = False
     screen = pygame.display.set_mode((500,500))
     ph = AntPheromone("test", 500, 500)
-    ph.draw([(10,10), (10,11), (11,10),(11,11)],[255,255,255])
-
+    ph.draw([(10,10), (10,11), (11,10),(11,11)],(255,255,255))
+    i = 0
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+        ph.decay()
+        ph.draw([(np.random.randint(0,500), np.random.randint(0,500))],(np.random.randint(0,256),np.random.randint(0,256),np.random.randint(0,256)))
+        if i < 500:
+            ph.draw([(i, 15)], (255,255,255))
+        i = i+1
         pygame.surfarray.blit_array(screen, ph.array)
-        #screen.blit(array.image(),(0,0))
         pygame.display.flip()
